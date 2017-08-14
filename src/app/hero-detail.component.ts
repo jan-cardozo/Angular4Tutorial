2d1f23ae69fa9d24abfe9a7e447a1170482440df
Jan Cardozo <jcardozo@developerplace.com>
2017-08-14 17:06:49 -0400

    Mid chapter 6

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,5 +1,11 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { Location } from "@angular/common";
+
+import "rxjs/add/operator/switchMap";
+
 import { Hero } from "./hero";
+import { HeroService } from "./hero.service";
 
 @Component({
   selector: "hero-detail",
@@ -15,6 +21,12 @@ import { Hero } from "./hero";
   `
 })
 
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
 }
