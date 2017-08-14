ae0b1a55fc0e32ab979fce5b2f7e20c3b34e0b4e
Jan Cardozo <jcardozo@developerplace.com>
2017-08-13 22:15:19 -0400

    Progreso hasta el capitulo 4

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero.js";
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Spiderman' },
+  { id: 12, name: 'Wolverine' },
+  { id: 13, name: 'Deadpool' },
+  { id: 14, name: 'Storm' },
+  { id: 15, name: 'Hulk' },
+  { id: 16, name: 'Rogue' },
+  { id: 17, name: 'Gambit' },
+  { id: 18, name: 'Venom' },
+  { id: 19, name: 'Dr. Strange' },
+  { id: 20, name: 'Iron-Man' },
+];
